c5ccf6a5bd54cab5803336a36be44272f19113e2
egorps-official <nul339370@gmail.com>
2026-07-12 09:07:12 +0300

    Update fs.lua

--- FROS0P5/libs/fs.lua
+++ FROS0P5/libs/fs.lua
@@ -1,1 +1,18 @@
+local lib = {}
 
+local component = component or require("component")
+local invoke = component.invoke
+
+local bootaddr, invoke = computer.getBootAddress()
+local function loadfile(addr, file)
+    local handle = assert(invoke(addr, "open", file))
+    local buffer = ""
+    repeat
+        local data = invoke(addr, "read", handle, math.maxinteger or math.huge)
+        buffer = buffer .. (data or "")
+    until not data
+    invoke(addr, "close", handle)
+    return load(buffer, "=" .. file, "bt", _G)
+end
+
+return lib
