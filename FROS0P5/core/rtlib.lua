bf8b9837c8d97bcf7e62fb65582be8786bb18959
egorps-official <nul339370@gmail.com>
2026-07-12 13:07:09 +0300

    Update rtlib.lua

--- FROS0P5/core/rtlib.lua
+++ FROS0P5/core/rtlib.lua
@@ -4,3 +4,29 @@
 
 -- Real-Time Thread Library for FastRunOS kernel
 
+--[[
+Codes:
+0x{type}{scriptNum}{code}
+
+Code:
+  0x01 - Library:
+    0x01 - fs:
+      0x01 - INVALID_ADDRESS
+      0x02 - UNSCANNED_DISK
+      0x03 - LETTER_MAP_NOT_FOUND
+
+Message:
+  INVALID_ADDRESS - Invalid Address or not a disk
+  UNSCANNED_DISK - Attempt to get access to the disk before it gets scanned (by "scanDisks" method)
+  LETTER_MAP_NOT_FOUND - index "DisksAddrs" in config.lua wasn't found
+]]--
+
+local function getLog(code, msg, status)
+  return {
+    ["log"] = {
+      ["Msg"] = msg,
+      ["Code"] = code,
+      ["Status"] = status
+    }
+  }
+end
