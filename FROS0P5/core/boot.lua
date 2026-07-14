af858d60995c22e1f4f870c771d4d4576ab0773c
egorps-official <nul339370@gmail.com>
2026-07-14 20:45:49 +0300

    Update boot.lua

--- FROS0P5/core/boot.lua
+++ FROS0P5/core/boot.lua
@@ -3,3 +3,17 @@
 -- Licensed by MIT License
 
 -- FastRunOS Kernel
+
+--[[
+Code:
+  0x00 - Core:
+    0x00 - boot (kernel):
+      0x01 - FS_LIB_NOT_FOUND
+      0x02 - CONFIG_NOT_FOUND
+      0x03 - RT_LIB_NOT_FOUND
+
+Message:
+  FS_LIB_NOT_FOUND - File System Library wasn't found
+  CONFIG_NOT_FOUND - config.lua wasn't found
+  RT_LIB_NOT_FOUND - Real-Time Thread Library wasn't found
+]]--
