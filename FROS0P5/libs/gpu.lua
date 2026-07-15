7768efb785103cf66158a2fcbe7fc6b44b847352
egorps-official <nul339370@gmail.com>
2026-07-15 12:40:11 +0300

    Update gpu.lua

--- FROS0P5/libs/gpu.lua
+++ FROS0P5/libs/gpu.lua
@@ -1,1 +1,21 @@
+-- FastRunOS
+-- By egorps
+-- Licensed by MIT License
 
+-- GPU Library
+
+--[[
+Code:
+  0x00 - Core:
+    0x00 - boot (kernel):
+      0x01 - FS_LIB_NOT_FOUND
+      0x02 - CONFIG_NOT_FOUND
+      0x03 - RT_LIB_NOT_FOUND
+      0x04 - GPU_LIB_NOT_FOUND
+
+Message:
+  FS_LIB_NOT_FOUND - File System Library wasn't found
+  CONFIG_NOT_FOUND - config.lua wasn't found
+  RT_LIB_NOT_FOUND - Real-Time Thread Library wasn't found
+  GPU_LIB_NOT_FOUND - GPU Library wasn't found
+]]--
