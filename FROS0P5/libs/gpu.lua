8172b1ab303356df193697cc1d8e67371d59e1a2
egorps-official <nul339370@gmail.com>
2026-07-15 21:12:50 +0300

    Update gpu.lua

--- FROS0P5/libs/gpu.lua
+++ FROS0P5/libs/gpu.lua
@@ -6,16 +6,11 @@
 
 --[[
 Code:
-  0x00 - Core:
-    0x00 - boot (kernel):
-      0x01 - FS_LIB_NOT_FOUND
-      0x02 - CONFIG_NOT_FOUND
-      0x03 - RT_LIB_NOT_FOUND
-      0x04 - GPU_LIB_NOT_FOUND
-
+  0x01 - Library:
+    0x00 - gpu:
+      0x01 - INVALID_FILE
 Message:
-  FS_LIB_NOT_FOUND - File System Library wasn't found
-  CONFIG_NOT_FOUND - config.lua wasn't found
-  RT_LIB_NOT_FOUND - Real-Time Thread Library wasn't found
-  GPU_LIB_NOT_FOUND - GPU Library wasn't found
+  INVALID_FILE - File wasn't found
 ]]--
+
+
